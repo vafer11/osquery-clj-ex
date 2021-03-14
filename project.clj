1b3266034f40a8e6f116f82b33efcf3a19a31d25
vafer11 <vfernandez11@outlook.com>
2021-03-13 21:50:26 -0300

    First commit...

---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject osquery-clj-ex "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [ring/ring-json "0.5.0"]
+                 [vafer/osquery-clj "0.1.8-alpha"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler osquery-clj-ex.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.2"]]}})
---
+++ src/osquery_clj_ex/handler.clj
@@ -0,0 +1,26 @@
+(ns osquery-clj-ex.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.util.response :refer [response]]
+            [osquery-clj-ex.osquery-service :refer :all]))
+
+(defn make-response [res]
+  (-> res
+      (vec)
+      (response)))
+
+(defroutes app-routes
+  (GET "/api/users/" [] (make-response (get-logged-users)))
+  (GET "/api/user/:uid" [uid] (make-response (get-logged-user-by-id uid)))
+  (GET "/api/processes/" [] (make-response (get-processes)))
+  (GET "/api/process/:pid" [pid] (make-response (get-process-by-id pid)))
+  (GET "/api/os/" [] (make-response (get-os-version)))
+  (GET "/api/memory/" [] (make-response (get-memory-capacity)))
+  (route/not-found "Not Found"))
+
+(def app
+  (-> app-routes
+      (wrap-json-response)
+      (wrap-defaults site-defaults)))
